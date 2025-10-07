b0d889e7ebf73fdc623a2ec567849f4dda8077d6
Samuel Pacheco <samupachebq@gmail.com>
2025-10-07 11:49:44 +0000

    Sigeu sin funcionarme ademas de las revisiones en el codgio que otras co

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, 'src/app/.env') });
+require('dotenv').config({ path: path.resolve(__dirname, '.env') });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -25,12 +25,7 @@ const nextConfig = {
     '*.cluster-lu4mup47g5gm4rtyvhzpwbfadi.cloudworkstations.dev',
     '*.ngrok-free.app',
     '*.ngrok-free.dev',
-  ],
-  env: {
-    NEXT_PUBLIC_VAPID_PUBLIC_KEY: process.env.NEXT_PUBLIC_VAPID_PUBLIC_KEY,
-    VAPID_PRIVATE_KEY: process.env.VAPID_PRIVATE_KEY,
-    SELLER_REGISTRATION_CODE: process.env.SELLER_REGISTRATION_CODE,
-  }
+  ]
 };
 
 module.exports = nextConfig;
