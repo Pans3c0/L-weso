bb4f75e1b8db3b01900d85ffd64e483b1b04c168
Samuel Pacheco <samupachebq@gmail.com>
2025-10-07 09:34:37 +0000

    sigue sin funcionar revisa que puede bloquear el uso de notificaciones e

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&family=Roboto:wght@700&display=swap" rel="stylesheet" />
